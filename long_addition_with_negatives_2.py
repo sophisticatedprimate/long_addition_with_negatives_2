5be265b00e4d340464d0017a53de5f5d81a3fb0e
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 17:22:21 -0500

    Add files via upload

---
+++ long_addition_with_negatives_2.py
@@ -0,0 +1,139 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_addition_with_decimals_1.py for further information on adding with decimals in python:
+https://github.com/sophisticatedprimate/long_addition_with_negatives_1/blob/main/long_addition_with_negatives_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed
+    2. Navigate to a directory containing this file (long_addition_with_negatives_2.py)
+    3. Run the program with the following command: python3 long_addition_with_negatives_2.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1 
+sum = -75 + -856  
+print("{0:<5} + {1:<5} = {2:<6}".format(-75,-856,sum))  
+
+# Problem 2 
+sum = -215 + 694  
+print("{0:<5} + {1:<5} = {2:<6}".format(-215,694,sum))  
+
+# Problem 3 
+sum = 629 + -862  
+print("{0:<5} + {1:<5} = {2:<6}".format(629,-862,sum))  
+
+# Problem 4 
+sum = 135 + 521  
+print("{0:<5} + {1:<5} = {2:<6}".format(135,521,sum))  
+
+# Problem 5 
+sum = 988 + -6  
+print("{0:<5} + {1:<5} = {2:<6}".format(988,-6,sum))  
+
+# Problem 6 
+sum = -915 + 122  
+print("{0:<5} + {1:<5} = {2:<6}".format(-915,122,sum))  
+
+# Problem 7 
+sum = 488 + 828  
+print("{0:<5} + {1:<5} = {2:<6}".format(488,828,sum))  
+
+# Problem 8 
+sum = -86 + 837  
+print("{0:<5} + {1:<5} = {2:<6}".format(-86,837,sum))  
+
+# Problem 9 
+sum = 447 + -209  
+print("{0:<5} + {1:<5} = {2:<6}".format(447,-209,sum))  
+
+# Problem 10
+sum = 482 + -76  
+print("{0:<5} + {1:<5} = {2:<6}".format(482,-76,sum))  
+
+# Problem 11
+sum = 259 + 111  
+print("{0:<5} + {1:<5} = {2:<6}".format(259,111,sum))  
+
+# Problem 12
+sum = 28 + 355  
+print("{0:<5} + {1:<5} = {2:<6}".format(28,355,sum))  
+
+# Problem 13
+sum = -251 + -470  
+print("{0:<5} + {1:<5} = {2:<6}".format(-251,-470,sum))  
+
+# Problem 14
+sum = 608 + 365  
+print("{0:<5} + {1:<5} = {2:<6}".format(608,365,sum))  
+
+# Problem 15
+sum = 438 + 136  
+print("{0:<5} + {1:<5} = {2:<6}".format(438,136,sum))  
+
+# Problem 16
+sum = 328 + 565  
+print("{0:<5} + {1:<5} = {2:<6}".format(328,565,sum))  
+
+# Problem 17
+sum = -514 + -840  
+print("{0:<5} + {1:<5} = {2:<6}".format(-514,-840,sum))  
+
+# Problem 18
+sum = -999 + -961  
+print("{0:<5} + {1:<5} = {2:<6}".format(-999,-961,sum))  
+
+# Problem 19
+sum = -579 + 948  
+print("{0:<5} + {1:<5} = {2:<6}".format(-579,948,sum))  
+
+# Problem 20
+sum = -389 + -70  
+print("{0:<5} + {1:<5} = {2:<6}".format(-389,-70,sum))  
+
+# Problem 21
+sum = -978 + 83  
+print("{0:<5} + {1:<5} = {2:<6}".format(-978,83,sum))  
+
+# Problem 22
+sum = -928 + 120  
+print("{0:<5} + {1:<5} = {2:<6}".format(-928,120,sum))  
+
+# Problem 23
+sum = 963 + 535  
+print("{0:<5} + {1:<5} = {2:<6}".format(963,535,sum))  
+
+# Problem 24
+sum = -323 + 225  
+print("{0:<5} + {1:<5} = {2:<6}".format(-323,225,sum))  
+
+# Problem 25
+sum = -6 + -283  
+print("{0:<5} + {1:<5} = {2:<6}".format(-6,-283,sum))  
+
+# Problem 26
+sum = -581 + -349  
+print("{0:<5} + {1:<5} = {2:<6}".format(-581,-349,sum))  
+
+# Problem 27
+sum = -186 + -709  
+print("{0:<5} + {1:<5} = {2:<6}".format(-186,-709,sum))  
+
+# Problem 28
+sum = -32 + 672  
+print("{0:<5} + {1:<5} = {2:<6}".format(-32,672,sum))  
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_with_negatives_2/issues
+"""
